b619370b9a43bc31f4f1263421689c4182ecca5e
Kersti Maurer <kmaurer@klass2-06-l.intra.itcollege.ee>
2015-10-11 13:53:55 +0300

    add harjutus

--- src/teema1/Peamurdja2_ristmik.java
+++ src/teema1/Peamurdja2_ristmik.java
@@ -11,10 +11,47 @@ import lib.Foor;
 public class Peamurdja2_ristmik extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
+        System.out.println("ALGUS");
         Foor foor1 = new Foor(Foor.YLEMINE, primaryStage);
         Foor foor2 = new Foor(Foor.ALUMINE, primaryStage);
         Foor foor3 = new Foor(Foor.PAREM, primaryStage);
         Foor foor4 = new Foor(Foor.VASAK, primaryStage);
+        vertikaalne(foor1);
+        vertikaalne(foor2);
+        horisontaalne(foor3);
+        horisontaalne(foor4);
     }
-
+    public void vertikaalne(Foor foor){
+        System.out.println("VERT");
+        for(int o=0; o<5;o=o+1) {
+            System.out.println("VERT tsükkel");
+            foor.vahetaRohelist();
+            foor.paus(0.5);
+            foor.vahetaRohelist();
+            foor.vahetaKollast();
+            foor.paus(0.5);
+            foor.vahetaKollast();
+            foor.vahetaPunast();
+            foor.paus(0.5);
+            foor.vahetaPunast();
+            foor.vahetaKollast();
+            foor.paus(0.5);
+            foor.vahetaKollast();
+        }
+    }
+    public void horisontaalne(Foor foor){
+        for(int z=0; z<5;z=z+1){
+        foor.vahetaPunast();
+        foor.paus(0.5);
+        foor.vahetaPunast();
+        foor.vahetaKollast();
+        foor.paus(0.5);
+        foor.vahetaKollast();
+        foor.vahetaRohelist();
+        foor.paus(0.5);
+        foor.vahetaRohelist();
+        foor.vahetaKollast();
+        foor.paus(0.5);
+        foor.vahetaKollast();}
+    }
 }
